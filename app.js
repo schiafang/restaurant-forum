099574d46d934e852f35bec904d427424205983e
carey <schiafang@gmail.com>
2020-06-30 15:14:35 +0800

    feat: add signup-form verification and flash message

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models')
 const app = express()
 const PORT = 3000
 
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.successMsg = req.flash('successMsg')
+  res.locals.errorMsg = req.flash('errorMsg')
+  // console.log(res.locals)
+  next()
+})
 
 app.listen(PORT, () => {
-  db.sequelize.sync()
+  // db.sequelize.sync()
   console.log(`app is listening on http://localhost:${PORT}`)
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,15 +4,29 @@ const User = db.User
 
 const userController = {
   signUpPage: (req, res) => res.render('signup'),
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    })
-      .then(() => res.redirect('/signin'))
-  },
   signInPage: (req, res) => res.render('signin'),
+  signUp: (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    if (password !== confirmPassword) {
+      req.flash('errorMsg', '密碼與確認密碼必須相同')
+      return res.render('signup', { name, email, password })
+    }
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) {
+          req.flash('errorMsg', '此信箱已經註冊')
+          return res.render('signup', { name, email, password })
+        }
+        User.create({ name, email, password: hashPassword })
+          .then(() => {
+            req.flash('successMsg', '註冊成功')
+            res.redirect('/signin')
+          })
+      })
+  }
 }
 module.exports = userController
+
+
+

21a25211ace36afd6ec8bd3f72867fce497cb25f
carey <schiafang@gmail.com>
2020-07-01 13:09:02 +0800

    feat: add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,14 +2,25 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 
 const adminController = {
+  //瀏覽全部餐廳
   getRestaurants: (req, res) => {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
   },
+  //瀏覽指定餐廳
+  getRestaurant: (req, res) => {
+    const id = req.params.id
+    return Restaurant.findByPk(id, { raw: true }).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant })
+    })
+
+  },
+  //瀏覽新增頁面
   createRestaurant: (req, res) => {
     return res.render('admin/create')
   },
+  //新增餐廳資料
   postRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
     if (!req.body.name) {

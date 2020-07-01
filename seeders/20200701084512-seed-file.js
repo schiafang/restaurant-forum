cf7b6b3526e2c8944a403d7b695998ad411f7242
carey <schiafang@gmail.com>
2020-07-01 17:18:16 +0800

    feat:add user seeder and  use faker set up restaurant seeder

---
+++ seeders/20200701084512-seed-file.js
@@ -0,0 +1,41 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      isAdmin: true,
+      name: 'root',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      isAdmin: false,
+      name: 'user1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+    return queryInterface.bulkInsert('Restaurants',
+      Array.from({ length: 50 }).map(d =>
+        ({
+          name: faker.name.findName(),
+          tel: faker.phone.phoneNumber(),
+          address: faker.address.streetAddress(),
+          opening_hours: '08:00',
+          image: faker.image.imageUrl(),
+          description: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Users', null, {})
+    return queryInterface.bulkDelete('Restaurants', null, {});
+  }
+};

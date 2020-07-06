6bca940744274b7ec4cafcbbd15122b93b8e2729
carey <schiafang@gmail.com>
2020-07-06 17:59:18 +0800

    feat: add restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,20 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
-  }
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(restaurant =>
+        ({
+          ...restaurant.dataValues,
+          description: restaurant.dataValues.description.substring(0, 50),
+          categoryName: restaurant.Category.name
+        })
+      )
+      return res.render('restaurants', { restaurants: JSON.parse(JSON.stringify(data)) })
+    })
+  },
+
 }
 module.exports = restController
--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,8 @@ module.exports = (app, passport) => {
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
+  // app.get('/restaurants', restontroller.getRestaurants)
+
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 

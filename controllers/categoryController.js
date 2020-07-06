1e6a69174c9da2b7cb7582c32e7ab44add391092
carey <schiafang@gmail.com>
2020-07-06 16:28:45 +0800

    feat: add category upadte and delete feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,14 +3,32 @@ const Category = db.Category
 
 const categoryController = {
   getCategories: (req, res) => {
-    Category.findAll({ raw: true, nest: true }).then(categories => {
-      return res.render('admin/categories', { categories })
+    const id = req.params.id
+    return Category.findAll({ raw: true, nest: true }).then(categories => {
+      if (id) {
+        Category.findByPk(id, { raw: true, nest: true })
+          .then(category => res.render('admin/categories', { categories, category }))
+      } else res.render('admin/categories', { categories })
     })
   },
   postCategory: (req, res) => {
     const name = req.body.name
     if (!name) res.redirect('back')
     else Category.create({ name }).then(() => res.redirect('/admin/categories'))
+  },
+  putCategory: (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    if (!name) res.redirect('back')
+    return Category.findByPk(id)
+      .then(category => category.update({ name }))
+      .then(() => res.redirect('/admin/categories'))
+  },
+  deleteCategory: (req, res) => {
+    const id = req.params.id
+    return Category.findByPk(id)
+      .then(category => category.destroy())
+      .then(() => res.redirect('/admin/categories'))
   }
 }
 

a5bb4a50e582aa8216bc1650529040b0b84ffbc2
carey <schiafang@gmail.com>
2020-07-06 13:33:06 +0800

    feat: add admin category post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,6 +6,11 @@ const categoryController = {
     Category.findAll({ raw: true, nest: true }).then(categories => {
       return res.render('admin/categories', { categories })
     })
+  },
+  postCategory: (req, res) => {
+    const name = req.body.name
+    if (!name) res.redirect('back')
+    else Category.create({ name }).then(() => res.redirect('/admin/categories'))
   }
 }
 
